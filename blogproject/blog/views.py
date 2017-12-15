d011f67754afa14d5a61c58e9ecff6788a8ab0bb
rambo <thb20110718@126.com>
2017-12-15 20:13:43 +0800

    新增静态文件css/js

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,11 +2,15 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 
 def index(request):
     # return HttpResponse("欢迎访问我的博客首页！")
-    return render(request, 'blog/index.html', context={
-                      'title': '我的博客首页',
-                      'welcome': '欢迎访问我的博客首页'
-                  })
+
+    # return render(request, 'blog/index.html', context={
+    #                   'title': '我的博客首页',
+    #                   'welcome': '欢迎访问我的博客首页'
+    #               })
+    post_list = Article.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
